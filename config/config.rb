fcb5b16a50063e5846556d4d6554264f284a1812
rokhimin wahid <rokhim.whd@gmail.com>
2019-08-01 21:55:09 +0700

    Update config.rb

--- config/config.rb
+++ config/config.rb
@@ -1,7 +1,7 @@
 #twitter.com/rokhiminwahid
 #twitter.com/whdzera
-configatron.token = "NjAzNjU2OTQ0MzM0NjY3Nzc2.XULB5Q.FhYtCzvKy_Iu4-WJqvgx1Jx2SlI" # go to http://discordapp.com/developers/applications/ to get your bot token
+configatron.token = "yourToken" # go to http://discordapp.com/developers/applications/ to get your bot token
 configatron.prefix = "/"
 configatron.game = "check command bot | #{configatron.prefix}help"
-configatron.userid = 603656944334667776 # go to ur server -> right-click in ur profile -> copy id
+configatron.userid = 603656944334667676 # go to ur server -> right-click in ur profile -> copy id
 configatron.botname = "Discordrb-Heroku"
