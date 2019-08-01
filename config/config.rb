f90b885c542497a7e38b6b568dee72c97d3d3a24
rokhimin wahid <rokhim.whd@gmail.com>
2019-08-01 17:30:57 +0700

    Update config.rb

--- config/config.rb
+++ config/config.rb
@@ -1,6 +1,6 @@
 #twitter.com/rokhiminwahid
 #twitter.com/whdzera
-configatron.token = "NjAzNjU2OTQ0MzM0NjY3Nzc2.XUK6JA.cUkEtxuNTCA8x6I22S0wYS-sPqI" # go to http://discordapp.com/developers/applications/ to get your bot token
+configatron.token = "NjAzNjU2OTQ0MzM0NjY3Nzc2.XUK_QA.c3mRlCQYcd4DATXrixZANjPqMZE" # go to http://discordapp.com/developers/applications/ to get your bot token
 configatron.prefix = "/"
 configatron.game = "check command bot | #{configatron.prefix}help"
 configatron.userid = 603656944334667776 # go to ur server -> right-click in ur profile -> copy id
